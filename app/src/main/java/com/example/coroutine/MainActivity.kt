c950e0c39db6b832eda64fe412e3a307fe0830af
mistifikastor <menoftoday@yandex.ru>
2026-03-15 20:55:13 +0700

    coroutine

--- app/src/main/java/com/example/coroutine/MainActivity.kt
+++ app/src/main/java/com/example/coroutine/MainActivity.kt
@@ -1,21 +1,27 @@
-package com.example.coroutine // замените на ваш package
+package com.example.coroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
-import com.example.coroutine.databinding.MainBinding // обратите внимание: MainBinding, а не ActivityMainBinding
+import com.example.coroutine.databinding.MainBinding
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainBinding
-    private var count: Int = 1  // текущее значение ползунка
+    private var count: Int = 1
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
+    private suspend fun performTask(taskNumber: Int): Deferred<String> =
+        coroutineScope.async(Dispatchers.Main) {
+            delay(5000)
+            return@async "Finished Coroutine $taskNumber"
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Настройка слушателя для SeekBar
         binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 count = progress
